9a1ceb52cf805240a7f6d067a8aa0389b3df2726
BositAnvarov <abdulbosit2002@gmail.com>
2023-08-23 00:29:03 -0700

    need to finish footer

--- personal_portfolio/src/components/Footer/Footer.js
+++ personal_portfolio/src/components/Footer/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './Footer.css'; // Import your CSS file for styling
 
+
+
 function Footer() {
   return (
-    <footer className='footerStyle'>
-    &copy; {new Date().getFullYear()} Bosit Anvarov
+    <footer className='footer'>
+      <div className='footer-container'>
+
+        <div className='footer-data'>
+          <h2 className='footer-title'>Abdulbosit Anvarov</h2>
+          <p className='footer-text'>&copy; {new Date().getFullYear()}</p>
+        </div>
+
+        <div className='footer-data'>
+          <h2 className='footer-title'>EXPLORE</h2>
+          <ul className='footer-text'>
+            <li><a href="#home" className='footer-link'>HOME</a></li>
+            <li><a href="#skills" className='footer-link'>SKILLS</a></li>
+            <li><a href="#achievements" className='footer-link'>ACHIEVEMENTS</a></li>
+            <li><a href="#contact" className='footer-link'>CONTACT</a></li>
+          </ul>
+        </div>
+
+        <div className='footer-data'>
+          <h2 className='footer-title'>FOLLOW</h2>
+          <ul className='footer-text'>
+            <li><a href="#LinkedIn">LinkedIn</a></li>
+            <li><a href="#Github">GitHub</a></li>
+            <li><a href="#Email">Email</a></li>
+            <li><a href="#Phone">Phone</a></li>
+          </ul>
+        </div>
+
+      </div>
     </footer>
   );
 }
