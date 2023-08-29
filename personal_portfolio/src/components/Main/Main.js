00d0fc5f0c24c02eef7cc7029e21a9c8b5af00b1
BositAnvarov <abdulbosit2002@gmail.com>
2023-08-28 21:50:16 -0700

    wip

--- personal_portfolio/src/components/Main/Main.js
+++ personal_portfolio/src/components/Main/Main.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef } from 'react';
-import { motion, useInView, useAnimation } from 'framer-motion';
+import React from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
@@ -7,65 +6,23 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './Main.css'; // Import your CSS file for styling
 
-// Reusable component for applying motion animation
-function AnimatedComponent({ children }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref, {once: true});
-
-  const controls = useAnimation();
-
-  useEffect(() => {
-    console.log(isInView)
-    if (isInView) {
-      controls.start('visible');
-    }
-  }, [isInView, controls]);
-
-  const motionVariants = {
-    hidden: { opacity: 0, y: 75 },
-    visible: { opacity: 1, y: 0.25 },
-  };
-
-  return (
-    <motion.div
-      ref={ref}
-      variants={motionVariants}
-      initial='hidden'
-      animate={controls}
-      transition={{ duration: 0.5, delay: 0.25 }}
-    >
-      {children}
-    </motion.div>
-  );
-}
-
 function Main() {
   return (
     <main className='main'>
       {/* Home Component */}
-      <AnimatedComponent>
-        <Home />
-      </AnimatedComponent>
+      <Home />
 
       {/* About Component */}
-      <AnimatedComponent>
-        <About />
-      </AnimatedComponent>
+      <About />
 
       {/* Skills Component */}
-      <AnimatedComponent>
-        <Skills />
-      </AnimatedComponent>
+      <Skills />
 
       {/* Projects Component */}
-      <AnimatedComponent>
-        <Projects />
-      </AnimatedComponent>
+      <Projects />
 
       {/* Contact Component */}
-      <AnimatedComponent>
-        <Contact />
-      </AnimatedComponent>
+      <Contact />
     </main>
   );
 }
